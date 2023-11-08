eba9cddd4d56307eba49d3e457fb2bf7108f07ed
ARH <rugamer228@gmaiol.com>
2023-11-08 22:49:30 +1000

    start_bot и stop_bot были перемещены из main.py

--- core/handlers/basic.py
+++ core/handlers/basic.py
@@ -1,6 +1,15 @@
 from aiogram import Bot, types, Dispatcher
 from aiogram.filters.command import Command
+from core.settings import settings
 
 
 async def cmd_start(message: types.Message):
     await message.answer('Приф!!!! Твой id:' + str(message.from_user.id))
+
+
+async def start_bot(bot: Bot):
+    await bot.send_message(settings.bots.admin_id, f'<b>Бот был включён!</b>')
+
+
+async def stop_bot(bot: Bot):
+    await bot.send_message(settings.bots.admin_id, f'<b>Бот был остановлен!</b>')
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from core.handlers.basic import cmd_start
+from core.handlers.basic import cmd_start, start_bot, stop_bot
 from core.settings import settings
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=settings.bots.bot_token,
@@ -14,19 +14,8 @@ dp = Dispatcher()
 
 
 @dp.message.register(cmd_start)
-# f
-@dp.startup()
-async def start_bot(bot: Bot):
-    await bot.send_message(settings.bots.admin_id, f'<b>Бот был включён!</b>')
-
-
-@dp.shutdown()
-async def start_bot(bot: Bot):
-    await bot.send_message(settings.bots.admin_id, f'<b>Бот был остановлен!</b>')
-
-# Логика
-
-
+@dp.startup.register(start_bot)
+@dp.shutdown(stop_bot)
 async def main():
     await dp.start_polling(bot)
 if __name__ == "__main__":
